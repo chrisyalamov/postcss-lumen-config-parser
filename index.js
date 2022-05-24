1fa626df90f589b6bbb04bc3085a721a4baf60bd
Christian Yalamov <chrisyalamov@gmail.com>
2022-05-24 17:10:40 +0100

    Build passing

--- index.js
+++ index.js
@@ -4,13 +4,62 @@
 module.exports = (opts = {}) => {
   // Work with options here
 
+  // Array with the names of themes
+  let themes = Object.keys(opts.themes || {});
+
+  // Array with the names of components
+  let components = Object.keys(opts.components || {});
+
   return {
     postcssPlugin: 'postcss-lumen-config-parser',
-    /*
     Root (root, postcss) {
-      // Transform CSS AST here
+      root.walkAtRules(rule => {
+        if (rule.name === 'lumen' && rule.params === 'themes') {
+          // Create a rule for each theme
+          themes.forEach(theme => {
+            let selector = theme == "default" ? `:root` : `:root[data-theme="${theme}"]`;
+            let rule = postcss.rule({ selector });
+            // rule.append({ prop: '--theme', value: `"${theme}"` });
+            let themeConfig = opts.themes[theme];
+            // Iterating over the design tokens under the theme
+            Object.keys(themeConfig).forEach(prop => {
+              rule.append({ prop: `--${prop}`, value: themeConfig[prop] });
+            });
+            root.append(rule);
+          });
+          // Remove the lumen rule
+          rule.remove();
+        }
+        if (rule.name === 'lumen' && rule.params === 'components') {
+          // Create a rule for each component variant under each theme
+          components.forEach(component => {
+            let variants = Object.keys(opts.components[component] || {});
+            variants.forEach(variant => {
+              // For each variant...
+              themes.forEach(theme => {
+                // For each theme...
+
+                // E.g. :root[data-theme="brand-default"] .lds-button-danger
+                let selector = `${theme == "default" ? `:root` : `:root[data-theme="${theme}"]`} .lds-${component}.${variant}`;
+                let rule = postcss.rule({ selector });
+
+                Object.keys(opts.components[component][variant][theme] || {}).forEach(prop => {
+                  let value = opts.components[component][variant][theme][prop];
+                  let template = /\{([a-zA-Z0-9\-\_]*)\}/g.exec(value);
+                  if (template) {
+                    rule.append({ prop: `--lds-${prop}`, value: `var(--${template[1]})` });
+                  } else {
+                    rule.append({ prop: `--lds-${prop}`, value: value });
+                  }
+                });
+                root.append(rule);
+              })
+            })
+          })
+          rule.remove();
+        }
+      })
     }
-    */
 
     /*
     Declaration (decl, postcss) {
--- index.test.js
+++ index.test.js
@@ -2,16 +2,156 @@ const postcss = require('postcss')
 
 const plugin = require('./')
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
+async function run(input, output, opts = {}) {
+  let result = await postcss([plugin(opts)]).process(input, {
+    from: undefined
+  })
+  expect(result.css.trim()).toEqual(output.trim())
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
+let base = `
+@lumen themes;
+@lumen components;
+`
 
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
+it('creates CSS rules for themes', async () => {
+  let configuration = {
+    themes: {
+      default: {},
+      dark: {}
+    }
+  };
+
+  let expected = `
+:root {}
+:root[data-theme="dark"] {}`
+  await run(base, expected, configuration)
 })
 
-*/
+it('creates design tokens', async () => {
+  let configuration = {
+    themes: {
+      default: {
+        primary: '#000000',
+        secondary: '#ffffff'
+      },
+      dark: {
+        primary: '#ffffff',
+        secondary: '#000000'
+      }
+    }
+  };
+
+  let expected = `
+:root {
+    --primary: #000000;
+    --secondary: #ffffff
+}
+:root[data-theme="dark"] {
+    --primary: #ffffff;
+    --secondary: #000000
+}`
+  await run(base, expected, configuration)
+})
+
+it('creates CSS rules for component variants', async () => {
+  let configuration = {
+    themes: {
+      default: {},
+      dark: {}
+    },
+    components: {
+      button: {
+        success: {
+          default: {
+
+          },
+          dark: {
+
+          }
+        }
+      }
+    }
+  };
+
+  let expected = `
+:root {}
+:root[data-theme="dark"] {}
+:root .lds-button.success {}
+:root[data-theme="dark"] .lds-button.success {}`
+  await run(base, expected, configuration)
+})
+
+it('creates CSS declarations for component variant styles', async () => {
+  let configuration = {
+    themes: {
+      default: {},
+      dark: {}
+    },
+    components: {
+      button: {
+        success: {
+          default: {
+            bg: '#000000',
+            borderPrimary: '#000000'
+          },
+          dark: {
+            bg: '#ffffff',
+            borderPrimary: '#ffffff'
+          }
+        }
+      }
+    }
+  };
+
+  let expected = `
+:root {}
+:root[data-theme="dark"] {}
+:root .lds-button.success {
+    --lds-bg: #000000;
+    --lds-borderPrimary: #000000
+}
+:root[data-theme="dark"] .lds-button.success {
+    --lds-bg: #ffffff;
+    --lds-borderPrimary: #ffffff
+}`
+  await run(base, expected, configuration)
+})
+
+it('creates CSS declarations with reference values', async () => {
+  // E.g. converting "{green-500}" to "var(--green-500)"
+  let configuration = {
+    themes: {
+      default: {},
+      dark: {}
+    },
+    components: {
+      button: {
+        success: {
+          default: {
+            bg: '{green-500}',
+            borderPrimary: '{green-400}'
+          },
+          dark: {
+            bg: '{green-300}',
+            borderPrimary: '{green-600}'
+          }
+        }
+      }
+    }
+  };
+
+  let expected = `
+:root {}
+:root[data-theme="dark"] {}
+:root .lds-button.success {
+    --lds-bg: var(--green-500);
+    --lds-borderPrimary: var(--green-400)
+}
+:root[data-theme="dark"] .lds-button.success {
+    --lds-bg: var(--green-300);
+    --lds-borderPrimary: var(--green-600)
+}`
+  await run(base, expected, configuration)
+})
